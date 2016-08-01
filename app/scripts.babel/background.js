bda9d65464b92e9275470da5feae2d32dc18ffda
Arjun G <arjun@calicom.com>
2016-08-01 20:33:26 +0530

    Multiple updates

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -8,4 +8,41 @@ chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      if(request.event == 'SAVE'){
+          var bookmark = new VideoBookmark(
+              request.data._id,
+              request.data._time,
+              request.data._text,
+              request.data._url,
+              request.data._emoticon,
+              request.data._title,
+              request.data._color,
+              request.data._createdOn
+          );
+          bookmark.save();
+          sendResponse({});
+          sendRefreshMessage();
+      }
+      else if(request.event == 'GET'){
+          VideoBookmark.getAll(request.data)
+          .then(function(bookmarks){
+              sendResponse(bookmarks || []);
+          });
+          return true;
+      }
+  }
+);
+
+function sendRefreshMessage(){
+    chrome.tabs.query({}, function(tabs) {
+        tabs.forEach(function(tab) {
+            chrome.tabs.sendMessage(tab.id, {
+                event: 'REFRESH'
+            }, function(response) {
+                
+            });
+        }, this);
+    });
+}
---
+++ app/scripts.babel/utils.js
@@ -0,0 +1,24 @@
+function newGuid(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
+function formatTime(input){
+    if(!Math.round(input)){
+        return '00:00:00';
+    }
+    var totalSec = input;
+    var hours = parseInt( totalSec / 3600 ) % 24;
+    var minutes = parseInt( totalSec / 60 ) % 60;
+    var seconds = totalSec % 60;
+    return (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds  < 10 ? '0' + seconds : seconds);
+}
+
+function extractUrl(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    var id = (match&&match[7].length==11)? match[7] : false;
+    return 'https://www.youtube.com/watch?v=' + id;
+}
